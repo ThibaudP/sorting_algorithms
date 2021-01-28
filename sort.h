09e77e39722e68de4f2f1d944615d4d21fdb93b1
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-01-28 20:07:31 +0000

    feat(): add merge_sort algorithm

--- sort.h
+++ sort.h
@@ -36,5 +36,6 @@ void cocktail_sort_list(listint_t **list);
 /* ADVANCED SORT ALGORITHMS */
 void shell_sort(int *array, size_t size);
 void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
 
 #endif /* SORT_H */

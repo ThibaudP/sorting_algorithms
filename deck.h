3dc09659a6c32335bb9b6ed31d87d9df77e48640
ThibaudP <t.poncin@gmail.com>
2021-01-29 23:21:46 +0000

    feat(): Add bitonic_sort + everything Betty

--- deck.h
+++ deck.h
@@ -1,9 +1,24 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * enum kind_e - enum of the 4 card colors
+ *
+ * @SPADE: value 0
+ * @HEART: value 1
+ * @CLUB: value 2
+ * @DIAMOND: value 3
+ */
 typedef enum kind_e
 {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
 } kind_t;
 
 /**
@@ -13,10 +28,11 @@ typedef enum kind_e
  * From "Ace" to "King"
  * @kind: Kind of the card
  */
+
 typedef struct card_s
 {
-    const char *value;
-    const kind_t kind;
+	const char *value;
+	const kind_t kind;
 } card_t;
 
 /**
@@ -26,12 +42,17 @@ typedef struct card_s
  * @prev: Pointer to the previous node of the list
  * @next: Pointer to the next node of the list
  */
+
 typedef struct deck_node_s
 {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
 } deck_node_t;
+
+/* DECK SORT */
 
 void print_deck(const deck_node_t *deck);
 void sort_deck(deck_node_t **deck);
+
+#endif /* DECK_H */
